9b7c7284dce70f9eda2b7faa26230dff0ed938a8
Issamoxix <issamhaidaoui@gmail.com>
2020-03-21 17:16:01 +0100

    Update files.py

--- files.py
+++ files.py
@@ -1,8 +1,9 @@
+
 import os
 x = os.listdir()
 y = len(x)+1
-ext={'python':['.py']},{'pictures':['.png','.jpg','.jpeg']},{'Php':['.php']},{'javascript':['.js']},{'ht_cs':['.html','.css']}
-files=[{}]
+ext={'python':['.py']},{'pictures':['.png','.jpg','.jpeg','.PNG']},{'Php':['.php']},{'javascript':['.js']},{'ht_cs':['.html','.css']},{'music':'.mp3'},{'video':'.mp4'}
+files={}
 for arrays in ext:
 	for arrays_ext in arrays:
 		#the extension array
@@ -13,18 +14,12 @@ for arrays in ext:
 		for po in range(0,ext_array_n):	
 			#the extension
 			#print(ext_array[po])
-			files[0].update({arrays_ext:[]})
+			files.update({arrays_ext:[]})
 			for fi in x:
 				if ext_array[po] in fi:
 					#print('We have one here is ', arrays_ext, fi)
-					files[0][arrays_ext].append(fi)
-					print(fi)
-		if len(files[0][arrays_ext]) >0:
-			print(arrays_ext,' :',len(files[0][arrays_ext]))
-
+					files[arrays_ext].append(fi)
+					#print(fi)
+		
+		print(arrays_ext,' :',len(files[arrays_ext]))
 #print(files)
-		
-
-
-
-		
